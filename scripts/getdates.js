4960a62b04de33f9eb70d0df80dc2db7d1ff40b9
Josephats1 <bjerrykibs@gmail.com>
2025-03-07 11:53:30 +0300

    added album

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,11 @@ function updateLastModified() {
 }
 // Call the function when the page is fully loaded
 document.addEventListener("DOMContentLoaded", updateLastModified);
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-toggle");
+    const nav = document.querySelector(".nav");
+
+    menuButton.addEventListener("click", function () {
+        nav.classList.toggle("show");
+    });
+});
