c3f3ca6598b9835c9508e0ddd9acde873306db5e
Josephats1 <bjerrykibs@gmail.com>
2025-03-26 19:09:01 +0300

    nav worked

--- scripts/filter.js
+++ scripts/filter.js
@@ -65,14 +65,7 @@ function updateLastModified() {
 // Call the function when the page is fully loaded
 document.addEventListener("DOMContentLoaded", updateLastModified);
 // JavaScript to toggle the navigation menu and change the hamburger icon to 'X' when clicked
-document.getElementById("menu-toggle").addEventListener("click", function() {
-    const navMenu = document.getElementById("nav-menu");
-    const hamburger = document.getElementById("menu-toggle");
 
-    // Toggle the 'show' class for the nav and 'active' class for the hamburger
-    navMenu.classList.toggle("show");
-    hamburger.classList.toggle("active");
-});
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menu-toggle");
     const navMenu = document.getElementById("nav-menu");
